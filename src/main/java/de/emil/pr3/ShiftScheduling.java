a3248ee974ed7e8b0af099b3f715a5c19aaa740c
Luisa Braun <luisa.braun@htwsaar.de>
2026-01-19 09:48:19 +0100

    clean maven structure

---
+++ src/main/java/de/emil/pr3/ShiftScheduling.java
@@ -0,0 +1,5 @@
+package de.emil.pr3;
+
+public class ShiftScheduling {
+
+}
---
+++ src/test/java/de/emil/pr3/ShiftSchedulingTest.java
@@ -0,0 +1,11 @@
+package de.emil.pr3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ShiftSchedulingTest {
+
+
+}
